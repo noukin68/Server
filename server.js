cebe4a8291becac8b9011a9bc25160051e3b03e6
Noukin <noukin68@gmail.com>
2024-03-14 10:59:52 +0300

    fix86

--- server.js
+++ server.js
@@ -1000,8 +1000,10 @@ io.on('connection', (socket) => {
 
     console.log('RestartTimer: ' + targetUid);
 
-    // Отправляем событие 'restart-timer' только конкретному клиенту с указанным UID
-    socket.emit('restart-timer', { uid: targetUid });
+    if (socket.uid !== targetUid) {
+
+      io.to(targetUid).emit('timer-restarted', { uid: targetUid});
+    }
   });
 
 
