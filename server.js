8fd0177158a5a7d6058ddb515240e273e8212988
Noukin <noukin68@gmail.com>
2024-03-15 10:54:51 +0300

    fix90

--- server.js
+++ server.js
@@ -991,19 +991,9 @@ io.on('connection', (socket) => {
     }
   });
 
-  socket.on('restart-timer', ({ uid: targetUid }) => {
-    // Проверяем, существует ли комната с указанным UID
-    if (!io.sockets.adapter.rooms.has(targetUid)) {
-        socket.emit('error', 'UID not found');
-        return;
-    }
-
-    console.log('RestartTimer: ' + targetUid);
-
-    if (socket.uid !== targetUid) {
-
-      io.to(targetUid).emit('restart-timer', { uid: targetUid });
-    }
+  socket.on('test-completed', ({ uid: targetUid }) => {
+    console.log('Test completed');
+    io.to(targetUid).emit('restart-timer', { uid: targetUid });
   });
 
   socket.on('timer-finished', () => {
