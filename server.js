e73d960ce2d8dfcf9ff036543e3929fe7a60f3db
Noukin <noukin68@gmail.com>
2024-03-16 17:04:40 +0300

    fix97

--- server.js
+++ server.js
@@ -1070,54 +1070,36 @@ app.get('/notify', (req, res) => {
   res.send('Уведомление отправлено');
 });
 
-const registerUser = (username, email, password, callback) => {
-  bcrypt.hash(password, 10, (err, hash) => {
-    if (err) {
-      console.error('Ошибка хеширования пароля:', err);
-      return callback(err);
-    }
-
-    const sql = 'INSERT INTO users (username, email, password) VALUES (?, ?, ?)';
-    db.query(sql, [username, email, hash], (err, result) => {
-      if (err) {
-        console.error('Ошибка регистрации пользователя:', err);
-        return callback(err);
-      }
-      callback(null, email);
-    });
-  });
-};
-
-const addLicense = (email, callback) => {
-  const licenseSql = 'INSERT INTO licenses (email) VALUES (?)';
-  db.query(licenseSql, [email], (err, licenseResult) => {
-    if (err) {
-      console.error('Ошибка добавления лицензии:', err);
-      return callback(err);
-    }
-    callback(null);
-  });
-};
-
-app.post('/registerParent', (req, res) => {
+app.post('/registerparent', (req, res) => {
   const { username, email, password } = req.body;
 
   if (!username || !email || !password) {
     return res.status(400).json({ message: 'Пожалуйста, укажите имя пользователя, адрес электронной почты и пароль' });
   }
 
-  registerUser(username, email, password, (err, email) => {
+  bcrypt.hash(password, 10, (err, hash) => {
     if (err) {
+      console.error('Ошибка хеширования пароля:', err);
       return res.status(500).json({ message: 'Внутренняя ошибка сервера' });
     }
 
-    addLicense(email, (err) => {
+    const sql = 'INSERT INTO users (username, email, password) VALUES (?, ?, ?)';
+    db.query(sql, [username, email, hash], (err, result) => {
       if (err) {
+        console.error('Ошибка регистрации пользователя:', err);
         return res.status(500).json({ message: 'Внутренняя ошибка сервера' });
       }
 
-      console.log('Пользователь успешно зарегистрирован');
-      res.status(201).json({ message: 'Пользователь успешно зарегистрирован' });
+      const licenseSql = 'INSERT INTO licenses (email) VALUES (?)';
+      db.query(licenseSql, [email], (err, licenseResult) => {
+        if (err) {
+          console.error('Ошибка добавления лицензии:', err);
+          return res.status(500).json({ message: 'Внутренняя ошибка сервера' });
+        }
+
+        console.log('Пользователь успешно зарегистрирован');
+        res.status(201).json({ message: 'Пользователь успешно зарегистрирован' });
+      });
     });
   });
 });
