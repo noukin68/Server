c7d14313e23af110b49bfc1a9138eaf95dfef43d
Noukin <noukin68@gmail.com>
2024-03-16 15:57:58 +0300

    fix97

--- server.js
+++ server.js
@@ -27,8 +27,6 @@ db.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
   console.log('The solution is: ', results[0].solution);
 });
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
 app.use(cors());
 app.use(express.json());
 app.use((req, res, next) => {
