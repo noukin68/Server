50dc73f15d8016fdf8ff1e1ebce6a7e339a7c99e
Noukin <noukin68@gmail.com>
2024-04-02 18:37:05 +0300

    newServer5

--- server.js
+++ server.js
@@ -1033,18 +1033,17 @@ io.on('connection', (socket) => {
     }
   });
 
-
-
-  socket.on('restart-time', ({ uid: targetUid, timeInSeconds}) => {
-    if (!io.sockets.adapter.rooms.has(targetUid)) {
-      socket.emit('error', 'UID not found');
-      return;
+  socket.on('restart-timer', () => {
+    console.log('Запрос на перезапуск таймера');
+    if (!io.sockets.adapter.rooms.has(socket.uid)) {
+        socket.emit('error', 'UID not found');
+        return;
     }
-    if (socket.uid !== targetUid) {
-
-      io.to(targetUid).emit('restart-time', { uid: targetUid, timeInSeconds});
+    if(!timerStopped){
+      io.to(socket.uid).emit('time-received', { uid: socket.uid, timeInSeconds: clients[socket.uid].timeInSeconds });
     }
   });
+
 
   socket.on('process-data', ({ uid: targetUid, processes }) => {
     if (!io.sockets.adapter.rooms.has(targetUid)) {
