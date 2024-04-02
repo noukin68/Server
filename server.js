3b714ec94697bc1507b25e4a0a7387ea2ea96f65
Noukin <noukin68@gmail.com>
2024-04-02 20:23:39 +0300

    newServer5

--- server.js
+++ server.js
@@ -1033,27 +1033,20 @@ io.on('connection', (socket) => {
     }
   });
 
-  socket.on('restart-timer', () => {
+  socket.on('restart-timer', ({ uid: targetUid, timeInSeconds }) => {
     console.log('Запрос на перезапуск таймера');
-    if (!io.sockets.adapter.rooms.has(socket.uid)) {
+    if (!io.sockets.adapter.rooms.has(targetUid)) {
         socket.emit('error', 'UID not found');
         return;
     }
-    if(!timerStopped){
-      io.to(socket.uid).emit('time-received', { uid: socket.uid, timeInSeconds: clients[socket.uid].timeInSeconds });
-    }
-  });
-
-  socket.on('restart-time', ({ uid: targetUid, timeInSeconds}) => {
-    if (!io.sockets.adapter.rooms.has(targetUid)) {
-      socket.emit('error', 'UID not found');
-      return;
-    }
     if (socket.uid !== targetUid) {
-
-      io.to(targetUid).emit('restart-time', { uid: targetUid, timeInSeconds});
+        io.to(targetUid).emit('restart-time', { uid: targetUid, timeInSeconds });
+    } else {
+        if (!timerStopped) {
+            io.to(targetUid).emit('time-received', { uid: targetUid, timeInSeconds: clients[targetUid].timeInSeconds });
+        }
     }
-  });
+});
 
   socket.on('process-data', ({ uid: targetUid, processes }) => {
     if (!io.sockets.adapter.rooms.has(targetUid)) {
