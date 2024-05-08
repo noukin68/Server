ec72b0952a074afb9ffa58d77e61e8928fac5dcb
Noukin <noukin68@gmail.com>
2024-05-08 10:13:20 +0300

    fixcert3

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const mysql = require('mysql')
 const cors = require('cors')
-const http = require('http')
+const https = require('https')
 const socketIo = require('socket.io')
 const bcrypt = require('bcrypt')
 const { v4: uuidv4 } = require('uuid')
@@ -22,7 +22,13 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage })
 
 const app = express()
-const server = http.createServer(app)
+const server = https.createServer(
+	{
+		key: fs.readFileSync(path.join(__dirname, 'cert', 'key.pem')),
+		cert: fs.readFileSync(path.join(__dirname, 'cert', 'cert.pem')),
+	},
+	app
+)
 const io = socketIo(server)
 
 const port = 3000
@@ -1242,7 +1248,7 @@ app.post('/userregister', (req, res) => {
 })
 
 // Маршрут для аутентификации пользователя
-app.post('/userlogin', (req, res) => {
+app.post('/userlogin', cors(), (req, res) => {
 	const { email, password } = req.body
 
 	// Проверка наличия обязательных полей в запросе
