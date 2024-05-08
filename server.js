d3b39097f16591e4beea726558c3dff052e9f729
Noukin <noukin68@gmail.com>
2024-05-08 12:39:16 +0300

    fixcert4

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.use('/uploads', cors(), express.static('uploads'))
 app.use(cors())
 app.use(express.json())
 app.use((req, res, next) => {
-	res.setHeader('Access-Control-Allow-Origin', 'https://techproguide.ru')
+	res.setHeader('Access-Control-Allow-Origin', '*')
 	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
 	res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
 	next()
