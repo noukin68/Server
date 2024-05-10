ce00a8ce1b98ae3a4bdbf60780c7b24230c24a82
Noukin <noukin68@gmail.com>
2024-05-10 22:16:53 +0300

    fixcert6

--- server.js
+++ server.js
@@ -32,5 +32,5 @@ https
 		res.writeHead(200, { 'Content-Type': 'text/plain' })
 		res.end('Hello World\n')
 	})
-	.listen(443, 'techproguide.store')
+	.listen(443, '62.217.182.138')
 console.log('Server running at https://62.217.182.138')
