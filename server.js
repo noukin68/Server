a4a08f56d302db55beb2752841192726cd3485aa
Noukin <noukin68@gmail.com>
2024-05-10 20:36:59 +0300

    fixcert4

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const mysql = require('mysql')
 const cors = require('cors')
-const http = require('http')
+const https = require('https')
 const socketIo = require('socket.io')
 const bcrypt = require('bcrypt')
 const { v4: uuidv4 } = require('uuid')
@@ -10,10 +10,10 @@ const bodyParser = require('body-parser')
 const fs = require('fs')
 const nodemailer = require('nodemailer')
 
-http
+https
 	.createServer(function (req, res) {
 		res.writeHead(200, { 'Content-Type': 'text/plain' })
 		res.end('Hello World\n')
 	})
 	.listen(8080, '62.217.182.138')
-console.log('Server running at http://62.217.182.138:8080')
+console.log('Server running at https://62.217.182.138:8080')
